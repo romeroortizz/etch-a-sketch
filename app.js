acc9693f7062d640c5e30b64fa2d91f11ebac0de
Romero <175451535+romeroortizz@users.noreply.github.com>
2025-02-09 12:43:52 -0600

    resizable grid functionality

--- app.js
+++ app.js
@@ -8,18 +8,43 @@ let defaultGrid = defaultGridX * defaultGridY
 let DEFAULT_COLOR = "#000000"; // black
 const canvasWidth = canvas.clientWidth ;
 const canvasHeight = canvas.clientHeight;
-
+let allBoxes = canvas.querySelectorAll('.box')
 /*buttons */
 const clearbtn = document.querySelector('.clear');
 const colorbtn = document.querySelector('.color');
 const erasebtn = document.querySelector('.erase');
+const removeBorders = document.querySelector('.remove-grid')
+const rainbowbtn = document.querySelector('.rainbow')
 /*states */
 let eraseState = false;
 
+/*range slider */
+const rangeSlider = document.querySelector('.slider')
+const gridSize = document.querySelector('.grid-measure')
 
-console.log(canvasHeight,canvasWidth)
+console.log(canvasHeight, canvasWidth)
+
+function changeCanvasSize() {
+   
+
+    const rangeValue = rangeSlider.value;
+    gridSize.textContent = `${rangeValue} X ${rangeValue}`
+    defaultGridX = rangeValue;
+    defaultGridY = rangeValue;
+    defaultGridCount = rangeValue;
+    defaultGrid = defaultGridCount * defaultGridCount
+
+    displayBoxes()
+    hoverColor()
+    eraseBoxes()
+    clearAll()
+    
+}
+
 
 function displayBoxes() {
+    let allBoxes = canvas.querySelectorAll('.box')
+    canvas.innerHTML = '';
     for (let i = 0; i < defaultGrid; i++){
         const newBox = document.createElement('div');
         // css styles to add appropriate size to boxes
@@ -27,17 +52,20 @@ function displayBoxes() {
         newBox.style.height = `${canvasHeight / defaultGridCount}px`
         newBox.classList.add("box");
         canvas.appendChild(newBox)
+      
     }
-  
+   
+    
 }
 
 displayBoxes()
 
 
 
-const allBoxes = document.querySelectorAll('.box')
 
 function hoverColor() {
+   
+    let allBoxes = canvas.querySelectorAll('.box')
     allBoxes.forEach(box => {
         box.addEventListener('mouseover', () => {
             console.log("hover")
@@ -47,13 +75,14 @@ function hoverColor() {
     })
 }
 
-
 hoverColor()
 
+
 function eraseBoxes() {
+    
+    let allBoxes = canvas.querySelectorAll('.box')
     erasebtn.addEventListener('click', () => {
         console.log('erase')
-        eraseState = true;
 
       
             allBoxes.forEach(box => {
@@ -68,8 +97,9 @@ function eraseBoxes() {
 }
 
 eraseBoxes()
-function clearAll() {
 
+function clearAll() {
+    let allBoxes = canvas.querySelectorAll('.box')
     clearbtn.addEventListener('click', () => {
         console.log("clear")
 
@@ -81,6 +111,16 @@ function clearAll() {
 
 clearAll()
 
+function clearBorders() {
+    console.log('hi')
+    let allBoxes = canvas.querySelectorAll('.box');
 
+    allBoxes.forEach(box => {
+        box.classList.toggle('no-borders')
+    })
+}
 
 colorbtn.addEventListener('click', hoverColor)
+rangeSlider.addEventListener('input', changeCanvasSize )
+
+removeBorders.addEventListener('click',clearBorders)
